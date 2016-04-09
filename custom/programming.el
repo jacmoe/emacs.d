480c5afee542734e46d1678507d587e315934cb6
Jacob Moen <jacmoe.dk@gmail.com>
2016-04-09 08:55:22 +0200

    Cpp and PHP setup - irony mode, cmake-ide, rtags, php-refactor and flymake/flycheck..

--- custom/programming.el
+++ custom/programming.el
@@ -35,3 +35,23 @@
 
 ;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Irony
+;;(add-hook 'c++-mode-hook 'irony-mode)
+;;(add-hook 'c-mode-hook 'irony-mode)
+;;(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+;; (defun my-irony-mode-hook ()
+;;   (define-key irony-mode-map [remap completion-at-point]
+;;     'irony-completion-at-point-async)
+;;   (define-key irony-mode-map [remap complete-symbol]
+;;     'irony-completion-at-point-async))
+;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; Cmake IDE
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/rtags/")
+(require 'rtags) ;; optional, must have rtags installed
+(cmake-ide-setup)
