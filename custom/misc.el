c3e8a77f63ceb81423348934ca6cc5d23a8f2612
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-19 12:49:22 +0100

    Magit filenotify and magithub. Plus some convenience shortcuts for buffers

--- custom/misc.el
+++ custom/misc.el
@@ -16,3 +16,9 @@
 (setq create-lockfiles nil)
 
 (setq make-backup-files nil) ; Don't want any backup files
+
+;; easy keys to split window. Key based on ErgoEmacs keybinding
+(global-set-key (kbd "M-3") 'delete-other-windows) ; expand current pane
+(global-set-key (kbd "M-4") 'split-window-below) ; split pane top/bottom
+(global-set-key (kbd "M-2") 'delete-window) ; close current pane
+(global-set-key (kbd "M-s") 'other-window) ; cursor to other pane
