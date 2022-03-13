3d7835e3653d91c1930b1da07308a38c8de01de9
Jacob Moena <jacmoe.dk@gmail.com>
2022-03-13 20:03:10 +0100

    wc-mode is back

--- init.el
+++ init.el
@@ -300,6 +300,7 @@
 ;; Org-mode
 ;; Transparency
 ;; Gurumode
+;; Wc-mode
 ;; Acewindow
 ;; Magit
 ;; Org2blog
@@ -554,6 +555,22 @@
     (guru-global-mode t)
     (guru-mode t)
 )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Wc-mode
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package wc-mode
+  :bind
+  ("C-c w" . wc-mode))
+
+(defun novel-count-words (&optional begin end)
+  "count words between BEGIN and END (region); if no region defined, count words in buffer"
+  (interactive "r")
+  (let ((b (if mark-active begin (point-min)))
+      (e (if mark-active end (point-max))))
+    (message "Word count: %s" (how-many "\\w+" b e))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
